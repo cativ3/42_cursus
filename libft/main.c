2e9c2c60f9f95382ac1b424fff6674ae60e24f5f
Yusuf Özel <yusufozell97@gmail.com>
2024-11-10 10:12:08 +0300

    resolved

--- libft/main.c
+++ libft/main.c
@@ -5,8 +5,15 @@
 
 int main(int argc, char** argv)
 {
+<<<<<<< Updated upstream
 	char *s1 = "aaloremaa";
 	char *s2 = ft_strtrim(s1, "a");
 	printf("%s|\n", s2);
+=======
+	char c = 126;
+	printf("%d\n", isprint(c));
+	printf("%d\n", ft_isprint(c));
+
+>>>>>>> Stashed changes
 	return 0;
 }
